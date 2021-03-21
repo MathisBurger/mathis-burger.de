843890a554ba3e49ea6dfda8ea8ccda09f655a92
Mathis <handymathis@gmail.com>
2021-03-21 17:41:06 +0100

    added profile picture display

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,14 @@ export default function Home() {
     <div>
       <Header active={"home"} />
       <div className={style.bg}>
-
+          <div className={style.container}>
+              <div>
+                  text
+              </div>
+              <div className={style.profileBox}>
+                <div className={style.profilePictureBox} />
+              </div>
+          </div>
       </div>
     </div>
   )
