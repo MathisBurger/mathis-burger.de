362800f0c007e786d4f8e2d69e2dedc0620b3e86
Mathis <handymathis@gmail.com>
2021-03-21 22:10:58 +0100

    added base for projects page

---
+++ pages/projects.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "../components/Header";
+import style from "../styles/Projects.module.css";
+
+export default function Projects() {
+    return (
+        <div>
+            <Header active={"projects"} />
+            <div className={style.container}>
+
+            </div>
+        </div>
+    );
+}
