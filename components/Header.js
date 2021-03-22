4ad56d460d113e39c39a64e651865fb61cbfb159
Mathis <handymathis@gmail.com>
2021-03-22 18:28:53 +0100

    made header partly responsive

--- components/Header.js
+++ components/Header.js
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, {useState} from 'react';
 import style from '../styles/Header.module.css';
 import Image from "next/image";
 import Link from "next/link";
 
 export default function Header(props) {
+
+    const [showDropdown, changeShowDropdown] = useState(false);
 
     return (
         <div className={style.container}>
@@ -15,16 +17,20 @@ export default function Header(props) {
                     height={50}
                 />
             </div>
-            <Link href={"/"}>
-                <div className={`${style.navBox} ${props.active === "home" ? style.active: ''}`}>
-                    Home
-                </div>
-            </Link>
-            <Link href={"/projects"}>
-                <div className={`${style.navBox} ${props.active === "projects" ? style.active: ''}`}>
-                    Projects
-                </div>
-            </Link>
+            <div className={style.dropdown} onClick={() => changeShowDropdown(!showDropdown)} />
+            <div className={style.linkBox}>
+                <Link href={"/"}>
+                    <div className={`${style.navBox} ${props.active === "home" ? style.active: ''} ${showDropdown ? style.blockDisplay : style.noneDisplay}`}>
+                        Home
+                    </div>
+                </Link>
+                <Link href={"/projects"}>
+                    <div className={`${style.navBox} ${props.active === "projects" ? style.active: ''} ${showDropdown ? style.blockDisplay : style.noneDisplay}`}>
+                        Projects
+                    </div>
+                </Link>
+            </div>
         </div>
     );
 }
+
