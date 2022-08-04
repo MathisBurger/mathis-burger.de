1a0f5490b621d3076756567285562666adb573e6
Mathis Burger <mburger@die-netzwerkstatt.de>
2022-08-04 14:21:59 +0200

    [TASK] Added imprint to homepage

--- pages/impressum.tsx
+++ pages/impressum.tsx
@@ -3,24 +3,31 @@ import Header from "../components/Header";
 import Footer from "../components/Footer";
 import Wrapper from "../components/Wrapper";
 import React from "react";
+import styles from "../styles/Impressum.module.scss";
 
 const Impressum: NextPage = () => {
 
     return (
         <Wrapper>
             <Header active="" />
-            Impressum
-            Angaben gemäß § 5 TMG
+            <div className={styles.container}>
+                <h1>Impressum</h1>
+                <p>Angaben gemäß § 5 TMG</p>
+                <div />
 
-            Max Muster
-            Musterweg
-            12345 Musterstadt
-            Vertreten durch:
-            Max Muster
-            Kontakt:
-            Telefon: 01234-789456
-            Fax: 1234-56789
-            E-Mail: max@muster.de
+                <p>Mathis Burger</p>
+                <p>Mühlenberg 27</p>
+                <p>25794 Pahlen</p>
+                <div />
+                <p>Kontakt:</p>
+                <div/>
+                <p>E-Mail: kontakt@mathis-burger.de</p>
+                <div />
+                <p>Haftung für Links</p>
+
+                <p>Unser Angebot enthält Links zu externen Webseiten Dritter, auf deren Inhalte wir keinen Einfluss haben. Deshalb können wir für diese fremden Inhalte auch keine Gewähr übernehmen. Für die Inhalte der verlinkten Seiten ist stets der jeweilige Anbieter oder Betreiber der Seiten verantwortlich. Die verlinkten Seiten wurden zum Zeitpunkt der Verlinkung auf mögliche Rechtsverstöße überprüft. Rechtswidrige Inhalte waren zum Zeitpunkt der Verlinkung nicht erkennbar. Eine permanente inhaltliche Kontrolle der verlinkten Seiten ist jedoch ohne konkrete Anhaltspunkte einer Rechtsverletzung nicht zumutbar. Bei Bekanntwerden von Rechtsverletzungen werden wir derartige Links umgehend entfernen.
+                </p>
+                </div>
             <Footer />
         </Wrapper>
     );
