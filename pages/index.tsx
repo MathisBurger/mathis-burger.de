6ce7445ab009069a8357d9f78ca850069ad7686e
Mathis Burger <mburger@die-netzwerkstatt.de>
2023-07-31 23:27:19 +0200

    [TASK] Updated landing page with new animations and background

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,7 @@ import { useEffect, useState } from 'react';
 import LoadingSpinner from '../components/LoadingSpinner';
 import Wrapper from '../components/Wrapper';
 import Footer from '../components/Footer';
+import Image from "next/image";
 
 /**
  * The index page that sows some general content of the page.
@@ -20,8 +21,20 @@ const Home = () => {
   return (
     <Wrapper>
       <Header active={'home'} />
-      <div className={style.bg}>
-      </div>
+      <div className={style.bg} />
+        <div className={style.container}>
+            <div className={style.imageRow}>
+                <img src="/me.jpg" alt=""/>
+                <img src="/me2.JPG" alt=""/>
+                <img src="/instagram.png" alt=""/>
+                <img src="/twitter.png" alt=""/>
+                <img src="/github.png" alt=""/>
+                <img src="/linked-in.png" alt=""/>
+                <img src="/logo.jpg" alt=""/>
+            </div>
+            <h1>Mathis Burger</h1>
+            <p>{mainText === '' ? <LoadingSpinner /> : mainText}</p>
+        </div>
       <Footer />
     </Wrapper>
   );
