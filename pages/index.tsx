905576f4169f8f17576fb79b4941a7f67bef0c09
Mathis Burger <handymathis@gmail.com>
2024-02-29 21:31:25 +0100

    [TASK] Updated animation rules of index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,7 @@ import { useEffect, useState } from 'react';
 import LoadingSpinner from '../components/LoadingSpinner';
 import Wrapper from '../components/Wrapper';
 import Footer from '../components/Footer';
+import {getCookie, setCookie} from "typescript-cookie";
 
 /**
  * The index page that sows some general content of the page.
@@ -11,17 +12,29 @@ import Footer from '../components/Footer';
 const Home = () => {
   const [mainText, setMainText] = useState<string>('');
 
+  const [noAnimation, setNoAnimation] = useState<boolean>(false);
+
+    useEffect(() => {
+        setNoAnimation(getCookie('animate-index') === 'true');
+    }, []);
+
   useEffect(() => {
     fetch('/mainText.txt')
       .then((res) => res.text())
       .then((txt) => setMainText(txt));
   }, []);
 
+    useEffect(() => {
+        return () => {
+            setCookie('animate-index', true, {expires: new Date((new Date()).getTime() +(1000*10))});
+        }
+    }, []);
+
   return (
     <Wrapper>
       <Header active={'home'} />
       <div className={style.bg} />
-      <div className={style.container}>
+      <div className={`${style.container} ${noAnimation ? style.noAnimation : ''}`}>
         <div className={style.imageRow}>
           <img src="/me.jpg" alt="" />
           <img src="/me2.JPG" alt="" />
